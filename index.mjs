20a178f54d3135d3743af7a17b9c5b47f694946f
ALFONSOMUJICAS <alfonso.mujica.scheel@gmail.com>
2023-08-03 13:38:26 -0400

    primer commit

---
+++ index.mjs
@@ -0,0 +1,234 @@
+import express from "express";
+import hbs from "hbs";
+import bodyParser from "body-parser";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import https from "https";
+
+
+//recuperar ruta raiz
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+//fin recuperación ruta raiz
+
+
+
+const app = express();
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+let idGastos = 1;
+let roommates = [];
+let gastos = [];
+
+roommates = leerRommate();
+gastos = leerGastos();
+
+app.get("/", (req, res) => {
+ 
+  res.render("index", { roommates, gastos });
+});
+
+app.post("/agregar-gasto", (req, res) => {
+  const data = JSON.parse(Object.keys(req.body)[0]);
+
+  let roomer;
+  for (let i = 0; i < roommates.length; i++) {
+    if (roommates[i].id == data.room) {
+      roomer = roommates[i].nombre;
+    }
+  
+  }
+let gastoModel = {
+  id: uuidv4(),
+  idroomer: data.room,
+  roomer: roomer,
+  descripcion: data.descripcion,
+  monto: data.monto,
+};
+  gastos.push(gastoModel);
+  idGastos++;
+  calcularGastos()
+
+ res.render("index", {roommates ,gastos});
+});
+
+app.post("/agregarroomates", async (req, res) => {
+  const usuario = await generarUser()
+  let roomModel = {
+    id: uuidv4(),
+    nombre: usuario,
+    debe: 0,
+    recibe: 0
+  };
+  roommates.push(roomModel);
+  calcularGastos();
+  res.status(200).send("gerenado")
+});
+
+app.post("/get-gasto",  (req, res) => {
+  const id = req.body.gastoId;
+  encontrarGasto(res, id);
+});
+
+app.delete("/borrar-gasto", (req, res) => {
+  const id = req.body.gastoId;
+  gastos = gastos.filter((gasto) => {
+    return gasto.id != id;
+  })
+  calcularGastos()
+  res.status(200).send("borrado");
+})
+
+
+app.put("/editar-gasto", (req, res) => {
+  const data = JSON.parse(Object.keys(req.body)[0]);
+  gastos.forEach((gasto) => {
+    if (gasto.id == data.idGasto) {
+      gasto.descripcion = data.descripcion;
+      gasto.monto = data.monto;
+    }
+  });
+  calcularGastos();
+  res.status(200).send("editado");
+})
+
+app.listen(3000, () => {
+  console.log("Server on port 3000");
+});
+
+function generarUser(){
+    return new Promise((resolve, reject) => {
+      const url = 'https://randomuser.me/api/';
+      https.get(url, (res) => {
+        let body = '';
+        res.on('data', (chunk) => {
+          body += chunk;
+        });
+        res.on('end', () => {
+          try{
+            const usuario =
+              JSON.parse(body).results[0].name.first + " "+
+              JSON.parse(body).results[0].name.last;
+            resolve(usuario);
+          }catch{
+            reject(error)
+          }
+        
+        });
+      })
+})
+}
+function encontrarGasto(res,id){
+gastos.forEach((gasto) => {
+  if (gasto.id == id) {
+    res.status(200).send(gasto);
+  } 
+});
+    res.status(404).send("no encontrado");
+}
+
+
+function calcularGastos() {
+
+  let cantidadUsuarios = roommates.length;
+
+  // Inicializar un objeto para almacenar el total a recibir por cada usuario
+  let totalRecibePorUsuario = {};
+
+  // Calcular el total a recibir por cada usuario dividido entre la cantidad de usuarios
+  gastos.forEach((gasto) => {
+    let montoPorUsuario = gasto.monto / cantidadUsuarios;
+    if (!totalRecibePorUsuario[gasto.idroomer]) {
+      totalRecibePorUsuario[gasto.idroomer] = montoPorUsuario;
+    } else {
+      totalRecibePorUsuario[gasto.idroomer] += montoPorUsuario;
+    }
+  });
+
+  // Reiniciar los saldos de "recibe" y "debe" para todos los usuarios
+  roommates.forEach((user) => {
+    user.recibe = (totalRecibePorUsuario[user.id] * (cantidadUsuarios-1) )|| 0;
+    user.debe = 0;
+  });
+
+  // Calcular los saldos de "debe" para cada usuario
+  roommates.forEach((user) => {
+    roommates.forEach((otherUser) => {
+      if (user.id !== otherUser.id) {
+        user.debe += totalRecibePorUsuario[otherUser.id] || 0;
+      }
+    });
+  });
+
+  roommates.forEach((user) => {
+    if (user.recibe >= user.debe) {
+      user.recibe -= user.debe;
+      user.debe = 0;
+    } else {
+      user.debe -= user.recibe;
+      user.recibe = 0;
+    }
+  });
+  crearArchivos();
+}
+
+function leerGastos() {
+  
+
+   fs.readFile("./data/gastos.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error al leer el archivo gastos.json:", err);
+    } else {
+      gastos = JSON.parse(data);
+    }
+  });
+
+  return gastos
+}
+function leerRommate(){
+  fs.readFile("./data/roommates.json", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error al leer el archivo roommates.json:", err);
+    } else {
+      roommates = JSON.parse(data);
+    }
+  });
+  return roommates
+}
+
+
+function crearArchivos(){
+  fs.writeFile("./data/roommates.json", JSON.stringify(roommates), (err) => {
+    if (err) {
+      console.log("Error al escribir el archivo roommates.json:", err);
+    } else {
+      console.log("Archivo roommates.json guardado exitosamente.");
+    }
+  })
+
+  fs.writeFile("./data/gastos.json", JSON.stringify(gastos), (err) => {
+    if (err) {
+      console.log("Error al escribir el archivo gastos.json:", err);
+    } else {
+      console.log("Archivo gastos.json guardado exitosamente.");
+    }
+  })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/js/index.js
@@ -0,0 +1,125 @@
+const btnAgregar = document.getElementById("agregarRoomates");
+const agregarGasto = document.getElementById("agregarGasto");
+const btnEditar = document.getElementById("editar");
+
+
+const enviarPost = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({}), 
+};
+
+btnAgregar.addEventListener("click", () => {
+  fetch("/agregarroomates", enviarPost).then((response) => {
+    if (response.status === 200) {
+      console.log("entro");
+      location.reload();
+    }
+  });
+});
+
+agregarGasto.addEventListener("click", () => {
+  const room = document.getElementById("room").value;
+  const descripcion = document.getElementById("descripcion").value;
+  const monto = document.getElementById("monto").value;
+
+  const data = { room, descripcion, monto }; // datos para enviar
+
+ const enviarPostGasto = {
+   method: "POST",
+   headers: {
+      'Content-Type': 'application/x-www-form-urlencoded', // si uno pone esto deja enviar un objeto
+   },
+   body: JSON.stringify(data), //se le carga el objeto al cuerpo del metodo post
+ };
+
+fetch("/agregar-gasto", enviarPostGasto)
+  .then((response) => {
+    if (response.status === 200) {
+      location.reload();
+    }
+  })
+  .catch((error) => {
+    console.error("Error en la solicitud fetch:", error);
+  });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const editarBotones = document.querySelectorAll(".editarGasto");
+  editarBotones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const gastoId = boton.dataset.id;
+      let envia = { gastoId };
+      let getGasto = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(envia),
+      };
+      fetch("/get-gasto", getGasto)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          }
+        }).then((data) => {
+          document.getElementById("roomModal").value = data.roomer;
+          document.getElementById("descripcionModal").value = data.descripcion;
+          document.getElementById("montoModal").value = data.monto;
+          document.getElementById("idGasto").value = data.id;
+        })
+        .catch((error) => {
+          console.error("Error en la solicitud fetch:", error);
+        });
+    });
+  });
+
+  //aquí mandamos el metodo para borrar
+const botonesBorrar = document.querySelectorAll(".borrarGasto");
+  botonesBorrar.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const gastoId = boton.dataset.id;
+      let envia = { gastoId };
+      let getBorrar = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(envia),
+      };
+      fetch("/borrar-gasto", getBorrar)
+        .then((response) => {
+          if (response.status === 200) {
+            location.reload();
+          }
+    });
+  });
+
+  })
+});
+
+btnEditar.addEventListener("click", () => {
+  const room = document.getElementById("roomModal").value;
+  const descripcion = document.getElementById("descripcionModal").value;
+  const monto = document.getElementById("montoModal").value;
+  const idGasto = document.getElementById("idGasto").value;
+
+  const data = { room, descripcion, monto, idGasto }; // datos para enviar
+
+ const enviarPostGasto = {
+   method: "PUT",
+   headers: {
+      'Content-Type': 'application/x-www-form-urlencoded', // si uno pone esto deja enviar un objeto
+   },
+   body: JSON.stringify(data), //se le carga el objeto al cuerpo del metodo post
+ };
+
+fetch("/editar-gasto", enviarPostGasto)
+  .then((response) => {
+    if (response.status === 200) {
+      location.reload();
+    }
+  })
+  .catch((error) => {
+    console.error("Error en la solicitud fetch:", error);
+  })
+  })
